b74f6d3ca32187a613e53d04df7301a3f7448ac2
ML-Mounika <159158311+ML-Mounika@users.noreply.github.com>
2024-06-18 23:14:01 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,22 @@
-provider "aws" {
-  region = "us-east-1"
-
-}
-
-resource "aws_s3_bucket" "example" {
-  bucket = "my-tf-test-bucket9234"
-
-  tags = {
-    Name        = "My bucket"
-    Environment = "Dev"
+terraform {
+	  backend "s3" {
+    bucket = "my-tf-test-bucket9234"
+    key    = "munika/awstest/terraform.tfstate"
   }
 }
+
+provider "aws" {
+region = "us-west-1"
+}
+
+resource "aws_s3_bucket" "fd_bucket" {
+  bucket = "fd-9234-bucket"  # Replace with your desired bucket name
+  acl    = "private"
+  }
+
+output "s3_bucket_id" {
+  description = "The name of the bucket."
+  value       = aws_s3_bucket.fd_bucket.id
+}
+
+
