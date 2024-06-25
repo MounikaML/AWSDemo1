098de6391a02b775942749b2cf9897a5d0004073
ML-Mounika <159158311+ML-Mounika@users.noreply.github.com>
2024-06-25 20:32:50 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,14 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-    }
-  }
-}
-
-provider "aws" {
-  region      = "us-east-1"
-}
 resource "aws_instance" "instance1" {
   ami           = "ami-08a0d1e16fc3f61ea"
   instance_type = "t2.micro"
