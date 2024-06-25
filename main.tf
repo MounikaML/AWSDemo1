0f812f9fffdffa479bd5f49cf6c387f358fbd86c
ML-Mounika <159158311+ML-Mounika@users.noreply.github.com>
2024-06-25 17:30:13 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 terraform {
 	  backend "s3" {
     bucket = "my-tf-test-bucket9234"
-    key    = "mounika/awsDemo/terraform.tfstate"
+    key    = "MLMounika/awsDemo/terraform.tfstate"
   }
 }
 provider "aws" {
